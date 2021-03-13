9f1e1bf6eac3fbb8716257d3b706d93ecc99c54d
cmoyano <cristianmoyano.mza@gmail.com>
2021-03-13 20:10:30 -0300

    add content

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -59,7 +59,19 @@ const IndexPage = ({ data }) => {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         <h3 id="skills">My Skills</h3>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        Through the last years, I've gained a solid understanding of web development concepts, and I've dedicated a lot of my time to apply these concepts to real-world scenarios at my job.
+        <br/><br/>
+        <ul>
+          <li>Git</li>
+          <li>Python</li>
+          <li>Celery</li>
+          <li>Django</li>
+          <li>React</li>
+          <li>MySQL</li>
+          <li>API</li>
+          <li>English & Spanish</li>
+          <li>Mentorship</li>
+        </ul>
       </Content>
       <Content style={aboutStyle}>
         <h2 id="projects">What I've been working on</h2>
@@ -67,7 +79,9 @@ const IndexPage = ({ data }) => {
       </Content>
       <Content style={aboutStyle}>
         <h2 id="contact">Let's build something together</h2>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        Feel free to reach me out if you're looking for a developer, have a question, or just want to connect.
+        <br/><br/>
+        <a className="contact" href="mailto:cristianmoyano.mza@gmail.com">cristianmoyano.mza@gmail.com</a>
       </Content>
     </Layout>
   )
