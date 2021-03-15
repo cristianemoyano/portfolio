26a1ff33e23cffdb79e17e4e73e63a8c4f52b456
cmoyano <cristianmoyano.mza@gmail.com>
2021-03-15 19:07:29 -0300

    remove previous analytics implementation

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -11,12 +11,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-KLMKXJZ8SK",
-      },
-    },
-    {
       resolve: "gatsby-plugin-google-gtag",
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
